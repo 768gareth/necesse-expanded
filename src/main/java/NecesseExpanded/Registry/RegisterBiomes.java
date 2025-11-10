56f9dd7e0aee1b6d0c336fda00e746b13f4feca3
GC <gareth2.chappell@live.uwe.ac.uk>
2025-11-10 01:39:38 +0000

    Work in progression implementation of Tropical Biome. Tree spawn rate needs tweaking. Must implement pirate outposts, and new pirate village presets.

--- src/main/java/NecesseExpanded/Registry/RegisterBiomes.java
+++ src/main/java/NecesseExpanded/Registry/RegisterBiomes.java
@@ -1,6 +1,7 @@
 package NecesseExpanded.Registry;
 
 import NecesseExpanded.World.Biomes.FishianDeepCaveBiome;
+import NecesseExpanded.World.Biomes.TropicalBiome;
 import necesse.engine.registries.BiomeRegistry;
 
 public class RegisterBiomes 
@@ -10,5 +11,6 @@ public class RegisterBiomes
         System.out.println("[Necesse Expanded] Registering new biomes...");
 
         BiomeRegistry.registerBiome("fishian_cave", new FishianDeepCaveBiome(), true);
+        BiomeRegistry.registerBiome("tropical_biome", new TropicalBiome().setGenerationWeight(0.75f), true);
     }
 }
---
+++ src/main/java/NecesseExpanded/World/Biomes/TropicalBiome.java
@@ -0,0 +1,234 @@
+package NecesseExpanded.World.Biomes;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import necesse.engine.AbstractMusicList;
+import necesse.engine.MusicList;
+import necesse.engine.registries.MusicRegistry;
+import necesse.engine.registries.ObjectLayerRegistry;
+import necesse.engine.registries.ObjectRegistry;
+import necesse.engine.registries.TileRegistry;
+import necesse.engine.sound.SoundSettings;
+import necesse.engine.sound.SoundSettingsRegistry;
+import necesse.engine.util.GameRandom;
+import necesse.engine.util.LevelIdentifier;
+import necesse.engine.world.biomeGenerator.BiomeGeneratorStack;
+import necesse.engine.world.biomeGenerator.GeneratorPlaceFactory;
+import necesse.entity.mobs.PlayerMob;
+import necesse.level.gameObject.GameObject;
+import necesse.level.gameTile.GameTile;
+import necesse.level.maps.Level;
+import necesse.level.maps.biomes.Biome;
+import necesse.level.maps.biomes.FishingLootTable;
+import necesse.level.maps.biomes.FishingSpot;
+import necesse.level.maps.biomes.MobSpawnTable;
+import necesse.level.maps.presets.RandomCaveChestRoom;
+import necesse.level.maps.presets.caveRooms.CaveRuins;
+import necesse.level.maps.regionSystem.Region;
+
+public class TropicalBiome extends Biome 
+{
+    public static FishingLootTable SurfaceFish = new FishingLootTable().addAll(Biome.defaultSurfaceFish);
+    public static FishingLootTable CaveFish = new FishingLootTable().addAll(Biome.defaultCaveFish);
+    public static FishingLootTable DeepCaveFish = new FishingLootTable().addWater(100, "heartfish");
+    public static MobSpawnTable SurfaceMobs = new MobSpawnTable().add(25, "zombie");
+    public static MobSpawnTable CaveMobs = new MobSpawnTable().add(25, "zombie");
+    public static MobSpawnTable DeepCaveMobs = new MobSpawnTable().add(25, "zombie");
+    public static MobSpawnTable SurfaceCritters = new MobSpawnTable()
+    .add(100, "swampslug")
+    .add(100, "crab")
+    .add(80, "frog")
+    .add(40, "bird")
+    .add(40, "cardinalbird")
+    .add(40, "duck");
+    public static MobSpawnTable CaveCritters = new MobSpawnTable().add(100, "crab");
+    public static MobSpawnTable DeepCaveCritters = new MobSpawnTable().add(100, "crab");
+
+    public TropicalBiome() {  }
+
+    @Override
+    public boolean canRain(Level level) 
+    {
+        return true;
+    }
+
+    @Override
+    public FishingLootTable getFishingLootTable(FishingSpot Spot) 
+    {
+        if (Spot.tile.level.getIdentifier() == LevelIdentifier.DEEP_CAVE_IDENTIFIER)
+        {
+            return DeepCaveFish;
+        }
+        else if (Spot.tile.level.isCave)
+        {
+            return CaveFish;
+        }
+        else
+        {
+            return SurfaceFish;
+        }
+    }
+
+    @Override
+    public MobSpawnTable getCritterSpawnTable(Level Level) 
+    {
+        if (Level.getIdentifier() == LevelIdentifier.DEEP_CAVE_IDENTIFIER)
+        {
+            return DeepCaveCritters;
+        }
+        else if (Level.isCave)
+        {
+            return CaveCritters;
+        }
+        else
+        {
+            return SurfaceCritters;
+        }
+    }
+
+    @Override
+    public MobSpawnTable getMobSpawnTable(Level Level) 
+    {
+        if (Level.getIdentifier() == LevelIdentifier.DEEP_CAVE_IDENTIFIER)
+        {
+            return DeepCaveMobs;
+        }
+        else if (Level.isCave)
+        {
+            return CaveMobs;
+        }
+        else
+        {
+            return SurfaceMobs;
+        }
+    }
+
+    @Override
+    public AbstractMusicList getLevelMusic(Level level, PlayerMob perspective) 
+    {
+        return new MusicList(MusicRegistry.ByTheField);
+    }
+
+    @Override
+    public GameTile getUnderLiquidTile(Level level, int tileX, int tileY) 
+    {
+        return TileRegistry.getTile(TileRegistry.sandID);
+    }
+
+    @Override
+    public int getGenerationWaterTileID() {
+
+        return TileRegistry.waterID;
+    }
+
+    @Override
+    public int getGenerationCaveLavaTileID() 
+    {
+        return TileRegistry.waterID;
+    }
+
+    @Override
+    public int getGenerationDeepCaveLavaTileID() 
+    {
+        return TileRegistry.waterID;
+    }
+
+    @Override
+    public int getGenerationTerrainTileID() 
+    {
+        return TileRegistry.overgrownGrassID;
+    }
+
+    @Override
+    public int getGenerationCaveRockObjectID() 
+    {
+        return ObjectRegistry.sandstoneRockID;
+    }
+
+    @Override
+    public int getGenerationCaveTileID() 
+    {
+        return TileRegistry.waterID;
+    }
+
+    public int getGenerationBeachTileID() 
+    {
+        return TileRegistry.sandID;
+     }
+
+    public SoundSettings getWindSound(Level level) 
+    {
+    return SoundSettingsRegistry.wind;
+    }
+
+    @Override
+    public void initializeGeneratorStack(BiomeGeneratorStack stack) 
+    {
+        super.initializeGeneratorStack(stack);
+        stack.addRandomSimplexVeinsBranch("tropicalPalmTrees", 2.0F, 0.2F, 0.4F, 0);
+        stack.addRandomVeinsBranch("tropicalBushes", 0.045F, 4, 8, 0.6F, 0, false);
+        stack.addRandomSimplexVeinsBranch("tropicalMudPatches", 2.0F, 0.5F, 0.7F, 2);
+    }
+
+    @Override
+    public void generateRegionSurfaceTerrain(Region region, BiomeGeneratorStack stack, GameRandom random) 
+    {
+        super.generateRegionSurfaceTerrain(region, stack, random);
+        stack.startPlaceOnVein(this, region, random, "tropicalPalmTrees").onlyOnTile(TileRegistry.overgrownGrassID).chance(0.15).placeObject("palmtree");
+        stack.startPlaceOnVein(this, region, random, "tropicalPalmTrees").onlyOnTile(TileRegistry.overgrownGrassID).chance(0.03).placeObject("bambootree");
+        stack.startPlaceOnVein(this, region, random, "tropicalPalmTrees").onlyOnTile(TileRegistry.overgrownGrassID).chance(0.01).placeObject("bananatree");
+        stack.startPlaceOnVein(this, region, random, "tropicalBushes").onlyOnTile(TileRegistry.overgrownGrassID).placeObjectFruitGrower("blueberrybush");
+        stack.startPlaceOnVein(this, region, random, "tropicalBushes").onlyOnTile(TileRegistry.overgrownGrassID).placeObjectFruitGrower("blackberrybush");
+        final GameObject wildMushroom = ObjectRegistry.getObject("wildmushroom");
+        stack.startPlaceOnVein(this, region, random, "tropicalMudPatches").onlyOnTile(TileRegistry.overgrownGrassID)
+        .customPlace(new GeneratorPlaceFactory.RegionPlaceFunction() 
+        {
+            public void place(GameRandom random, Region region, int regionTileX, int regionTileY, Level level, int tileX, int tileY) 
+            {
+                if (random.getChance(0.6F))
+                {
+                    region.tileLayer.setTileByRegion(regionTileX, regionTileY, TileRegistry.mudID);
+                }
+                if (region.objectLayer.getObjectIDByRegion(ObjectLayerRegistry.BASE_LAYER, regionTileX, regionTileY) == 0 && random.getChance(0.15F) && wildMushroom.canPlace(level, tileX, tileY, 0, false) == null)
+                {
+                    wildMushroom.placeObject(level, tileX, tileY, 0, false);
+                }
+          }
+        });
+        stack.startPlace(this, region, random).chance(0.003).placeObject("surfacerock");
+        stack.startPlace(this, region, random).chance(0.005).placeObject("surfacerocksmall");
+        region.updateLiquidManager();
+        region.simulateWorldTime(1000000000, true);
+    }
+
+    @Override
+    public void generateRegionCaveTerrain(Region region, BiomeGeneratorStack stack, GameRandom random) {
+        super.generateRegionCaveTerrain(region, stack, random);
+        
+    }
+
+    @Override
+    public void generateRegionDeepCaveTerrain(Region region, BiomeGeneratorStack stack, GameRandom random) {
+        super.generateRegionDeepCaveTerrain(region, stack, random);
+        
+    }
+
+    @Override
+    public CaveRuins getNewCaveRuinsPreset(GameRandom random, AtomicInteger lootRotation) {
+        return null;
+    }
+
+    @Override
+    public CaveRuins getNewDeepCaveRuinsPreset(GameRandom random, AtomicInteger lootRotation) {
+        return null;
+    }
+
+    @Override
+    public RandomCaveChestRoom getNewCaveChestRoomPreset(GameRandom random, AtomicInteger lootRotation) {
+        return null;
+    }
+
+    @Override
+    public RandomCaveChestRoom getNewDeepCaveChestRoomPreset(GameRandom random, AtomicInteger lootRotation) {
+        return null;
+    }
+}
